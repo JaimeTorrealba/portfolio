36c73dea44ca62ccc28e95e92cbb32ceda317e96
Jaime Torrealba <solucionesinformaticasjtc@gmail.com>
2024-05-09 15:56:40 +0100

    add chains

--- src/components/webgl/PureNail.vue
+++ src/components/webgl/PureNail.vue
@@ -8,7 +8,9 @@ defineProps({
 
 const nailRef = shallowRef(null)
 
-const { scene } = await useGLTF('/models/Pure_Nail.glb', { draco: true})
+const { scene } = await useGLTF('/models/Pure_Nail.glb', { draco: true })
+const { scene: iron } = await useGLTF('/models/iron_chain.glb')
+const iron2 = iron.clone()
 
 watch(nailRef, (nail) => {
     nail.traverse((child) => {
@@ -19,20 +21,19 @@ watch(nailRef, (nail) => {
 })
 </script>
 <template>
-    <primitive ref="nailRef" name="nail" :scale="scaleFactor * 0.25" :position="[0, 2.5 * scaleFactor, 3]" :object="scene" >
-        <Sparkles 
-        :scale="scaleFactor"
-        :position="[-0.25, -12, 1.9 ]"
-        :sequence-alpha="[[0., 0.], [0.6, 1.0], [0.7, 0.0], [1.0, 1.0]]"
-        :sequence-color="['yellow', 'white', 'orange', 'red', 'black']" 
-        :sequence-offset="[[0.7, [0, 0, 0]], [0.75, [0, 0.1, 0]], [1.0, [0, 0.5, 0]]]" 
-        :sequence-size="[[0.0, 0.0], [0.7, 1.0]]" 
-        :sequence-surface-distance="[[0.0, 0.0], [0.7, 1.0]]" 
-        :lifetime-sec="3.0"
-        :size="2"
-        :surface-distance="0.8"
-        :mix-color="1.0"
-      />
+    <primitive ref="nailRef" name="nail" :scale="scaleFactor * 0.25" :position="[0, 2.5 * scaleFactor, 3]"
+        :object="scene">
+        <Sparkles :scale="scaleFactor" :position="[-0.25, -12, 1.9]"
+            :sequence-alpha="[[0., 0.], [0.6, 1.0], [0.7, 0.0], [1.0, 1.0]]"
+            :sequence-color="['yellow', 'white', 'orange', 'red', 'black']"
+            :sequence-offset="[[0.7, [0, 0, 0]], [0.75, [0, 0.1, 0]], [1.0, [0, 0.5, 0]]]"
+            :sequence-size="[[0.0, 0.0], [0.7, 1.0]]" :sequence-surface-distance="[[0.0, 0.0], [0.7, 1.0]]"
+            :lifetime-sec="3.0" :size="2" :surface-distance="0.8" :mix-color="1.0" />
     </primitive>
+    <TresGroup  >
+        <!--  -->
+        <primitive :object="iron"  :position="[-6,-1,-4]" :rotation-z="Math.PI * 0.15" />
+        <primitive :object="iron2"  :position="[6,-1,-6]" :rotation-z="-Math.PI * 0.20" />
+    </TresGroup>
     <BakeShadows />
 </template>
